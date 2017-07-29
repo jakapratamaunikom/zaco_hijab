9ebc3d31ebf94d3a0b8be449ec70962e56edc120
dmadan27 <rarasta27@gmail.com>
2017-07-29 20:02:51 +0700

    function/helper.php dan index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+	Define("BASE_PATH", true);
+	date_default_timezone_set('Asia/Jakarta');
+
+	include_once("function/helper.php");
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+		
+		<title>Sistem Informasi Zaco Hijab</title>
+		<!-- jQuery -->
+
+		<!-- CSS -->
+	</head>
+	<body class="hold-transition skin-blue sidebar-mini">
+
+		<div class="wrapper">
+			<!-- Header -->
+			<?php include_once("pages/header.php") ?>
+
+			<!-- Sidebar -->
+
+
+			<!-- Content -->
+
+
+			<!-- Footer -->
+
+
+			<!-- Control Sidebar -->
+
+
+		</div>
+		<!-- ./wrapper -->
+
+		<!-- JavaScript -->
+
+	</body>
+</html>
