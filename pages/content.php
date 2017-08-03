6d127d6b3beb53efe2f8d24e318a45ab547f3828
dmadan27 <rarasta27@gmail.com>
2017-08-03 14:11:38 +0700

    module/content

--- pages/content.php
+++ pages/content.php
@@ -1,30 +1,20 @@
 <?php
     Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
-    
+    $filename = "module/$m/$p.php";
 ?>
   
     <!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
 
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                Page Header
-                <small>Optional description</small>
-            </h1>
-            <ol class="breadcrumb">
-                <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
-                <li class="active">Here</li>
-            </ol>
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-
-          <!-- Your Page Content Here -->
-
-        </section>
-        <!-- /.content -->
+        <?php
+            // jika filenya ada
+            if(file_exists($filename)){
+                include_once($filename);
+            }
+            else{ // jika tidak ada
+                include_once("module/beranda/beranda.php");
+            }
+        ?>
 
     </div>
     <!-- /.content-wrapper -->
