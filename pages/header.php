58614d70bcfe5a9a4a5c4166a98849ba9d39e931
dmadan27 <rarasta27@gmail.com>
2017-08-03 23:59:48 +0700

    template/header

--- pages/header.php
+++ pages/header.php
@@ -9,7 +9,7 @@
 	    <!-- Logo -->
 	    <a href="index2.html" class="logo">
 	      	<!-- mini logo for sidebar mini 50x50 pixels -->
-	      	<span class="logo-mini"><b>A</b>LT</span>
+	      	<span class="logo-mini"><b>Z</b>H</span>
 	      	<!-- logo for regular state and mobile devices -->
 	      	<span class="logo-lg"><b>Zaco</b>Hijab</span>
 	    </a>
