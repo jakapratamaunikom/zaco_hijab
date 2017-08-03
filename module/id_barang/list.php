391eb8233ae3396790644ea3df3ac358447856e2
dmadan27 <rarasta27@gmail.com>
2017-08-03 15:03:00 +0700

    id_barang/list.php
    template isi konten list

--- module/id_barang/list.php
+++ module/id_barang/list.php
@@ -11,16 +11,65 @@
   		<link rel="stylesheet" type="text/css" href="<?= base_url."assets/plugins/DataTables/Responsive-2.1.1/css/responsive.bootstrap.min.css"; ?>"/>
 	<!-- -->
 
-	<!-- isi konten -->
+	<!-- header dan breadcrumb -->
 	<section class="content-header">
-      	<h1>
-        	Dashboard
-        	<small>Control panel</small>
-      	</h1>
+      	<h1>Id Barang</h1>
       	<ol class="breadcrumb">
-        	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        	<li class="active">Dashboard</li>
+        	<li><a href="<?= base_url ?>"><i class="fa fa-dashboard"></i> Zaco Hijab</a></li>
+        	<li>Data Master</li>
+        	<li class="active"><a href="<?= base_url."index.php?m=id_barang&p=list"; ?>">Id Barang</a></li>
+        	<li>Data Id Barang</li>
       	</ol>
+    </section>
+
+    <!-- isi konten -->
+    <section class="content">
+    	<div class="row">
+    		<div class="col-xs-12">
+    			<!-- panel box -->
+    			<div class="box">
+    				<!-- judul panel box -->
+    				<div class="box-header">
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-12">
+                                <h3 class="box-title">Data Id Barang</h3>
+                            </div>
+                        </div>
+                        <!-- panel button -->
+                        <div class="row" style="padding-top: 25px;">
+                            <div class="col-md-12 col-xs-12">
+                                <div class="btn-group">
+                                    <!-- tambah -->
+                                    <button type="button" class="btn btn-default">Tambah</button>
+                                    <!-- export excel -->
+                                    <button type="button" class="btn btn-default">Export Excel</button>
+                                    <!-- export pdf -->
+                                    <button type="button" class="btn btn-default">Export Pdf</button>
+                                </div>
+                            </div>
+                        </div>
+    				</div>
+    				<!-- isi panel box -->
+    				<div class="box-body">
+                        <!-- tabel -->
+                        <div class="row">
+                            <div class="col-md-12 col-xs-12">
+                                <table id="tabel_id_barang" class="table table-bordered table-hover dt-responsive nowrap" cellspacing="0" width="100%">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 15px">No</th>
+                                            <th>Id Barang</th>
+                                            <th>Nama</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
     </section>
 
     <!-- js -->
@@ -32,7 +81,7 @@
 		<script type="text/javascript">
 			//setting datatable
 			$(function(){
-				$("#tabelHarga").DataTable({
+				$("#tabel_id_barang").DataTable({
 					"language" : {
 						"lengthMenu": "Tampilkan _MENU_ data/page",
 			            "zeroRecords": "Data Tidak Ada",
