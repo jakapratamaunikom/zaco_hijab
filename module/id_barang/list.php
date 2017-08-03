f5b1b75bc57aa924108a6a4d77765eed8fd32827
dmadan27 <rarasta27@gmail.com>
2017-08-03 14:40:40 +0700

    id_barang

--- module/id_barang/list.php
+++ module/id_barang/list.php
@@ -5,3 +5,48 @@
 
 	<!-- List -->
 	
+	<!-- css -->
+		<!-- DataTables -->
+  		<link rel="stylesheet" type="text/css" href="<?= base_url."assets/plugins/DataTables/DataTables-1.10.15/css/dataTables.bootstrap.min.css"; ?>"/>
+  		<link rel="stylesheet" type="text/css" href="<?= base_url."assets/plugins/DataTables/Responsive-2.1.1/css/responsive.bootstrap.min.css"; ?>"/>
+	<!-- -->
+
+	<!-- isi konten -->
+	<section class="content-header">
+      	<h1>
+        	Dashboard
+        	<small>Control panel</small>
+      	</h1>
+      	<ol class="breadcrumb">
+        	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        	<li class="active">Dashboard</li>
+      	</ol>
+    </section>
+
+    <!-- js -->
+    	<!-- DataTables -->
+		<script type="text/javascript" src="<?= base_url."assets/plugins/DataTables/DataTables-1.10.15/js/jquery.dataTables.min.js"; ?>"></script>
+		<script type="text/javascript" src="<?= base_url."assets/plugins/DataTables/DataTables-1.10.15/js/dataTables.bootstrap.min.js"; ?>"></script>
+		<script type="text/javascript" src="<?= base_url."assets/plugins/DataTables/Responsive-2.1.1/js/dataTables.responsive.min.js"; ?>"></script>
+		<script type="text/javascript" src="<?= base_url."assets/plugins/DataTables/Responsive-2.1.1/js/responsive.bootstrap.min.js"; ?>"></script>
+		<script type="text/javascript">
+			//setting datatable
+			$(function(){
+				$("#tabelHarga").DataTable({
+					"language" : {
+						"lengthMenu": "Tampilkan _MENU_ data/page",
+			            "zeroRecords": "Data Tidak Ada",
+			            "info": "Page _PAGE_ dari _PAGES_",
+			            "infoEmpty": "Data Kosong",
+			            "search": "Pencarian:",
+			            "paginate": {
+					        "first": "Pertama",
+					        "last": "Terakhir",
+					        "next": "Selanjutnya",
+					        "previous": "Sebelumnya"
+					    }
+					}
+				});
+			});
+		</script>
+    <!-- -->
