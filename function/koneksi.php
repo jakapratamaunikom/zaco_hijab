4c3d338e8da77ae66a0d86049099029e2a5752cc
dmadan27 <rarasta27@gmail.com>
2017-08-07 20:42:58 +0700

    koneksi
    Perbaikan sedikit di bagian catch

--- function/koneksi.php
+++ function/koneksi.php
@@ -10,4 +10,6 @@
 		// atur error mode
 		$koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
-	catch(PDOException $e) die("Koneksi Database Error: " . $e->getMessage()); // jika ada error
+	catch(PDOException $e){
+		die("Koneksi Database Error: " . $e->getMessage()); // jika ada error
+	}
