199bd0feaa31755aedea2fc0fb74d6e3ac3aa82b
dmadan27 <rarasta27@gmail.com>
2017-08-07 20:25:44 +0700

    koneksi database
    => koneksi database
    => menggunakan PDO

---
+++ function/koneksi.php
@@ -0,0 +1,13 @@
+<?php
+	$dbHost = "localhost"; //host/server yg dipakai
+	$dbUser = "root"; //username host/server 
+	$dbPass = ""; //password host
+	$dbName = "zaco_hijab"; //nama database
+
+	try{
+		// buat object koneksi
+		$koneksi = new PDO("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPass);
+		// atur error mode
+		$koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e) die("Koneksi Database Error: " . $e->getMessage()); // jika ada error
