c96a6718214b6c276a040b79563f7b5b3e4229be
dmadan27 <rarasta27@gmail.com>
2017-08-10 15:43:58 +0700

    function/helper.php
    => pembuatan function cek duplikat
    => error handler jika ada pemasukan data yg duplikat

--- function/helper.php
+++ function/helper.php
@@ -1,3 +1,33 @@
 <?php
 	define("base_url", "http://localhost/zaco_hijab/");
 	define("version", "Alpha v0.1");
+
+	/* 
+		fungsi cek duplikat
+		=> akan mengembalikan nilai true jika ada yg duplikat
+		=> false jika tidak ada yg duplikat
+		=> $config_db berupa array yg isinya:
+			--> index tabel = tabel mana yg akan di cek
+			--> index field = field mana yg akan di cek
+			--> index value = nilai dari field yg akan di cek
+	*/
+	function cekDuplikat($koneksi, $config_db){
+		$tabel = $config_db['tabel'];
+		$field = $config_db['field'];
+		$value = $config_db['value'];
+
+		$query = "SELECT COUNT(*) FROM $tabel WHERE $field=?";
+
+		// prepare
+		$statement = $koneksi->prepare($query);
+		// bind
+		$statement->bindParam(1, $value);
+		// execute
+		$statement->execute();
+		$result = $statement->fetch();
+
+		if($result[0] > 0) $cek = true; // jika duplikat
+		else $cek = false; // jika tidak
+
+		return $cek;
+	}
