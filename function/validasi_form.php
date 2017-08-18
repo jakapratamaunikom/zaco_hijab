a581fd57f367febfb3abdd6f44f14f7e887c0d89
dmadan27 <rarasta27@gmail.com>
2017-08-18 18:23:09 +0700

    function/validasi_form
    => perapihan code
    => penyempurnaan di validTemplate
    => perubahan format dan penyempurnaan code di validHuruf, validAngka, validString
    => penambahan fungsi baru, validFoto
    => validFoto --> fungsi cek keabsahan file yg diupload, mengecek hanya bnr2 gambar.

--- function/validasi_form.php
+++ function/validasi_form.php
@@ -5,57 +5,131 @@
 			=> cek require --> cek kosong --> cek pattern --> cek min-max
 		# agar penulisan fungsi yg lainnnya lebih mudah
 	*/
-	function validTemplate($label, $value, $min, $max, $required, $cek, $error, $pattern){
-		// cek required
-		if($required){ // jika wajib
-			// cek kosong
-			if(empty($value)){ // jika kosong
-				$setError = $error['empty'];
-				$cek = false;
-			}
-			else{
-				// cek pattern
-				if(!preg_match($pattern, $value)){ // jika tidak sesuai
-					$setError = $error['pattern'];
-					$cek = false;
+	function validTemplate($cekValid){
+		$required = $cekValid['required'];
+		$value = $cekValid['value'];
+		$min = $cekValid['min'];
+		$max = $cekValid['max'];
+		$label = $cekValid['label'];
+		$cek = $cekValid['cek'];
+		$error = $cekValid['error'];
+		$pattern = $cekValid['pattern'];
+		$jenis = $cekValid['jenis'];
+
+		// cek jenis
+		switch (strtolower($jenis)) {
+			case 'string':
+			case 'huruf':
+				// cek required
+				if($required){ // jika wajib
+					// cek kosong
+					if(empty($value)){ // jika kosong
+						$setError = $error['empty'];
+						$cek = false;
+					}
+					else{
+						// cek pattern
+						if(!preg_match($pattern, $value)){ // jika tidak sesuai
+							$setError = $error['pattern'];
+							$cek = false;
+						}
+						else{
+							// cek min-max karakter
+							if(strlen($value) >= $min && strlen($value) <= $max){ // jika sesuai
+								$setError = "";
+								$cek = true;
+							}
+							else{
+								$setError = $error['length'];
+								$cek = false;
+							}
+						}
+					}
 				}
-				else{
-					// cek min-max karakter
-					if(strlen($value) >= $min && strlen($value) <= $max){ // jika sesuai
+				else{ // jika opsional
+					// jika diisi cek pattern --> cek max
+					if(!empty($value)){ // jika diisi
+						// cek pattern
+						if(!preg_match($pattern, $value)){ // jika tidak sesuai
+							$setError = $error['pattern'];
+							$cek = false;
+						}
+						else{
+							// cek min-max karakter
+							if(strlen($value) <= $max){ // jika sesuai
+								$setError = "";
+								$cek = true;
+							}
+							else{
+								$setError = $error['length'];
+								$cek = false;
+							}
+						}
+					}
+					else{ // jika kosong
 						$setError = "";
 						$cek = true;
 					}
-					else{
-						$setError = $error['length'];
+				}
+			break;
+
+			case 'angka':
+				// cek required
+				if($required){ // jika wajib
+					// cek kosong
+					if(empty($value)){ // jika kosong
+						$setError = $error['empty'];
 						$cek = false;
 					}
+					else{
+						// cek pattern
+						if(!preg_match($pattern, $value)){ // jika tidak sesuai
+							$setError = $error['pattern'];
+							$cek = false;
+						}
+						else{
+							// cek min-max karakter
+							if($value >= $min && $value <= $max){ // jika sesuai
+								$setError = "";
+								$cek = true;
+							}
+							else{
+								$setError = $error['length'];
+								$cek = false;
+							}
+						}
+					}
 				}
-			}
-		}
-		else{ // jika opsional
-			// jika diisi cek pattern --> cek max
-			if(!empty($value)){ // jika diisi
-				// cek pattern
-				if(!preg_match($pattern, $value)){ // jika tidak sesuai
-					$setError = $error['pattern'];
-					$cek = false;
-				}
-				else{
-					// cek min-max karakter
-					if(strlen($value) <= $max){ // jika sesuai
+				else{ // jika opsional
+					// jika diisi cek pattern --> cek max
+					if(!empty($value)){ // jika diisi
+						// cek pattern
+						if(!preg_match($pattern, $value)){ // jika tidak sesuai
+							$setError = $error['pattern'];
+							$cek = false;
+						}
+						else{
+							// cek min-max karakter
+							if($value <= $max){ // jika sesuai
+								$setError = "";
+								$cek = true;
+							}
+							else{
+								$setError = $error['length'];
+								$cek = false;
+							}
+						}
+					}
+					else{ // jika kosong
 						$setError = "";
 						$cek = true;
 					}
-					else{
-						$setError = $error['length'];
-						$cek = false;
-					}
 				}
-			}
-			else{ // jika kosong
-				$setError = "";
-				$cek = true;
-			}
+				break;
+
+			default:
+				die();
+				break;
 		}
 			
 		$output = array(
@@ -82,12 +156,14 @@
 			'length' => "Panjang $label Min. $min dan Maks. $max Karakter",
 		);
 		$cekValid['value'] = trim($value);
-		$pattern = "/^[ \w#-]+$/";
+		$cekValid['label'] = trim($label);
+		$cekValid['min'] = $min;
+		$cekValid['max'] = $max;
+		$cekValid['pattern'] = "/^[a-zA-Z0-9-_,.!?' ]*$/";
+		$cekValid['required'] = $required;
+		$cekValid['jenis'] = "string";
 
-		$output = validTemplate(
-			$label, $cekValid['value'], $min, $max, 
-			$required, $cekValid['cek'], $cekValid['error'], $pattern
-		);
+		$output = validTemplate($cekValid);
 
 		return $output;
 	}
@@ -106,15 +182,17 @@
 		$cekValid['error'] = array(
 			'empty' => $label." Harus Diisi",
 			'pattern' => $label." Harus Berupa Angka",
-			'length' => "Panjang $label Min. $min dan Maks. $max Karakter",
+			'length' => "Nilai $label Min. $min dan Maks. $max",
 		);
 		$cekValid['value'] = trim($value);
-		$pattern = "/^[0-9]*$/";
+		$cekValid['label'] = trim($label);
+		$cekValid['min'] = $min;
+		$cekValid['max'] = $max;
+		$cekValid['pattern'] = "/^[0-9.]*$/";
+		$cekValid['required'] = $required;
+		$cekValid['jenis'] = "angka";
 
-		$output = validTemplate(
-			$label, $cekValid['value'], $min, $max, 
-			$required, $cekValid['cek'], $cekValid['error'], $pattern
-		);
+		$output = validTemplate($cekValid);
 
 		return $output;
 	}
@@ -136,11 +214,98 @@
 			'length' => "Panjang $label Min. $min dan Maks. $max Karakter",
 		);
 		$cekValid['value'] = trim($value);
-		$pattern = "/^[0-9]*$/";
+		$cekValid['label'] = trim($label);
+		$cekValid['min'] = $min;
+		$cekValid['max'] = $max;
+		$cekValid['required'] = $required;
+		$cekValid['jenis'] = "huruf";
+		$cekValid['pattern'] = "^[a-zA-Z\s]*$/";
 
-		$output = validTemplate(
-			$label, $cekValid['value'], $min, $max, 
-			$required, $cekValid['cek'], $cekValid['error'], $pattern
+		$output = validTemplate($cekValid);
+
+		return $output;
+	}
+
+	/*
+		fungsi validasi foto, mengecek yg di upload adalah benar2 foto
+
+	*/
+	function validFoto($configFoto){
+		$errorFile = $configFoto['error'];
+		$sizeFile = $configFoto['size'];
+		$tmp_nameFile = $configFoto['tmp_name'];
+		$max = $configFoto['max'];
+		// $path = $configFoto['path'];
+
+		// cek error value
+		switch ($errorFile) {
+			case UPLOAD_ERR_OK:
+				$cekValid['error'] = "";
+            	break;
+	        case UPLOAD_ERR_NO_FILE:
+	            $output = array(
+					'cek' => false,
+					'error' => "Upload Gagal, Tidak File Yang Terupload",
+				);
+				return $output;
+				break;
+
+	        case UPLOAD_ERR_INI_SIZE:
+	        case UPLOAD_ERR_FORM_SIZE:
+	            $output = array(
+					'cek' => false,
+					'error' => "Upload Gagal, Ukuran File Tidak Sesuai",
+				);
+				return $output;
+				break;
+
+	        default:
+	            $output = array(
+					'cek' => false,
+					'error' => "Upload Gagal, Error Tidak Diketahui",
+				);
+				return $output;
+				break;
+		}
+
+		// cek ukuran foto
+		// 2*1048576 (2 mb)
+		if($sizeFile > $max){
+			/*
+				lakukan resize ukuran foto (pengembangan)
+			*/
+			$output = array(
+				'cek' => false,
+				'error' => "Upload Gagal, Ukuran File Tidak Sesuai",
+			);
+			return $output;
+		}
+
+		// cek mime type file
+		$finfo = new finfo(FILEINFO_MIME_TYPE);
+		// jika format file ngaco
+		if(false === $ext = array_search(
+			$finfo->file($tmp_nameFile), 
+			array(
+	            'jpg' => 'image/jpeg',
+	            'png' => 'image/png',
+	            'gif' => 'image/gif',
+	        ), true))
+		{
+			$output = array(
+				'cek' => false,
+				'error' => "Upload Gagal, Format File Tidak Sesuai",
+			);
+			return $output;
+		}
+
+		// ganti nama file
+		$namaFileBaru = sprintf('%s.%s', sha1_file($tmp_nameFile), $ext);
+
+		$output = array(
+			'cek' => true,
+			'error' => "",
+			'namaFile' => $namaFileBaru,
 		);
 
 		return $output;
@@ -171,19 +336,3 @@
 
 		return $data;
 	}
-
-
-	// // uji coba
-	// $label = "Nama";
-	// $value = "kimocai";
-	// $min = 1;
-	// $max = 20;
-	// $required = false;
-
-	// $validNamaProv = validString(
-	// 			$label, 
-	// 			validInputan($value, false, false), 
-	// 			1, 225, true
-	// 		);
-
-	// var_dump($validNamaProv);
