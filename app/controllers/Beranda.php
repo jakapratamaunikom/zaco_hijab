8fdcab17b5a63e9cd4435d543898c4408f37434f
dmadan27 <rarasta27@gmail.com>
2017-09-14 19:09:05 +0700

    beranda

--- app/controllers/Beranda.php
+++ app/controllers/Beranda.php
@@ -1,4 +1,23 @@
 <?php
+	date_default_timezone_set('Asia/Jakarta');
 
-?>
+	// Load semua fungsi yang dibutuhkan
+	include_once("../function/helper.php");
+	include_once("../function/koneksi.php");
+	include_once("../function/validasi_form.php");
+	include_once("../function/datatable.php");
 
+	$action = isset($_POST['action']) ? $_POST['action'] : false;
+	if(!$action) die("Dilarang Akses Halaman Ini !!");
+	else{
+		switch ($action) {
+			case 'value':
+				# code...
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+	}
+	
--- app/views/beranda/dashboard.php
+++ app/views/beranda/dashboard.php
@@ -1,4 +1,8 @@
 <?php
+	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
 
+	$notif = isset($_SESSION['notif']) ? $_SESSION['notif'] : false;
+	unset($_SESSION['notif']);
 ?>
+
 
