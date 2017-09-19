111309ee7460c25318fb4bb5734817cc5a4979e0
dmadan27 <rarasta27@gmail.com>
2017-09-19 13:59:44 +0700

    controller/admin

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -45,10 +45,13 @@
 		foreach($data_admin as $row){
 			$status = $row['status'] == "1" ? "AKTIF" : "NONAKTIF";
 			$no_urut++;
-			$aksi = '<a role="button" class="btn btn-info btn-flat btn-sm" href="'.base_url.'index.php?m=admin&p=view&id='.$row["id"].'">Detail</a>';
-			$aksi .= '<button type="button" class="btn btn-success btn-flat dropdown-toggle" data-toggle="dropdown"><i class="fa fa-caret-down"></i> Edit</button>';
-			$aksi .= '<ul class="dropdown-menu"><li><a role="button" href="'.base_url.'index.php?m=admin&p=form&id='.$row["id"].'"><i class="fa fa-plus"></i> Edit Profil</a></li>';
+			$aksi = '<div class="btn-group">';
+			$aksi .= '<a role="button" class="btn btn-info btn-flat btn-sm" href="'.base_url.'index.php?m=admin&p=view&id='.$row["username"].'">Detail</a>';
+			$aksi .= '<div class="btn-group">';
+			$aksi .= '<button type="button" class="btn btn-success btn-sm btn-flat dropdown-toggle" data-toggle="dropdown"><i class="fa fa-caret-down"></i> Edit</button>';
+			$aksi .= '<ul class="dropdown-menu"><li><a role="button" href="'.base_url.'index.php?m=admin&p=form&id='.$row["username"].'"><i class="fa fa-plus"></i> Edit Profil</a></li>';
 			$aksi .= '<li><a role="button" onclick="edit_status('."'".$row["username"]."'".')"><i class="fa fa-plus"></i> Edit Status</a></li>';
+			$aksi .= '</ul></div></div>';
 			
 			$dataRow = array();
 			$dataRow[] = $no_urut;
@@ -73,82 +76,103 @@
 	}
 
 	function actionAdd($koneksi){
-		$username = isset($_POST['username']) ? $_POST['username'] : "";
-		$password = isset($_POST['password']) ? $_POST['password'] : "";
-		$confpassword = isset($_POST['confirm']) ? $_POST['confirm'] : "";
-		$email = isset($_POST['email']) ? $_POST['email'] : "";
-		$level = isset($_POST['level']) ? $_POST['level'] : "";
-		$nama = isset($_POST['nama']) ? $_POST['nama'] : "";
-		$notelp = isset($_POST['telp']) ? $_POST['telp'] : "";
-		$alamat = isset($_POST['alamat']) ? $_POST['alamat'] : "";
-		$foto = isset($_FILES['foto']) ? $_FILES['foto'] : "";
+		$dataForm = isset($_POST) ? $_POST : false;
+		$foto = isset($_FILES['foto']) ? $_FILES['foto'] : false;
 
-		// inisialisasi
-			$cek = true;
-			$status = false;
-			$errorDb = false;
-			$duplikat = false;
-			$usernameError = $passwordError = $confpasswordError = $emailError = $fotoError = "";
-			$levelError = $namaError = $notelpError = $alamatError  = "";
-			$pesanError = $set_value = "";
+		// validasi
+			// inisialisasi
+			$cekFoto = true;
+			$status = $errorDb = $duplikat = false;
 
-			$valid_username = validString("Username", $username, 5, 10, true);
-			$valid_password = validString("Password", $password, 6, 10, true);
+			$configData = configData($dataForm);
+			$validasi = set_validasi($configData);
+			$cek = $validasi['cek'];
+			$pesanError = $validasi['pesanError'];
+			$set_value = $validasi['set_value'];
 
-			// cek valid
-			if(!$valid_username['cek']){
+			if($foto){
+				$configFoto = array(
+					'error' => $foto['error'],
+					'size' => $foto['size'],
+					'name' => $foto['name'],
+					'tmp_name' => $foto['tmp_name'],
+					'max' => 2*1048576,
+				);
+				$valid_foto = validFoto($configFoto);
+				if(!$valid_foto['cek']){
+					$cek = false;
+					$pesanError['fotoError'] = $valid_foto['error'];
+				}
+				else $valueFoto = $valid_foto['namaFile'];
+			}
+			else $valueFoto = "";
+
+			// cek password
+			$validPassword = validPassword("Password", $dataForm['password'], $dataForm['confirm'], 6, 50);
+			if(!$validPassword['cek']){
 				$cek = false;
-				$usernameError = $valid_username['error'];
+				$pesanError['passwordError'] = $validPassword['error']['password'];
+				$pesanError['passwordError'] = $validPassword['error']['confirm'];
+			}
+			else{
+				$set_value['password'] = $validPassword['value']['password'];
+				$set_value['confirm'] = $validPassword['value']['confirm'];	
 			}
 
-			if(!$valid_password['cek']){
-				$cek = false;
-				$passwordError = $valid_password['error'];
-			}
-
-			if($password !== $confpassword){
-				$cek = false;
-				$confpasswordError = $passwordError = "Password dan Confirm Password tidak sama !";
-			}
-
-			$pesanError = array(
-				'usernameError' => $usernameError,
-				'passwordError' => $passwordError,
-				'confpasswordError' => $confpasswordError,
-
+		// ======================================= //
+		if($cek){
+			$dataForm = array(
+				'username' => validInputan($dataForm['username'], false, true),
+				'password' => password_hash($dataForm['password'], PASSWORD_BCRYPT),
+				'confirm' => validInputan($dataForm['confirm'], false, true),
+				'nama' => validInputan($dataForm['nama'], false, false),
+				'email' => validInputan($dataForm['email'], false, true),
+				'level' => validInputan($dataForm['level'], false, false),
+				'telp' => validInputan($dataForm['telp'], false, false),
+				'alamat' => validInputan($dataForm['alamat'], false, false),
 			);
-
-			$set_value = array(
-				'username' => $username,
-				'password' => $password,
-				'confirm' => $confpassword,
-
-			);
-
-		// ===========================================
-
-		if ($cek) {
-			$username = validInputan($username, false, true);
-			$password = validInputan($password, false, true);
 
 			// cek duplikat id barang
 			$config_duplikat = array(
 				'tabel' => 'admin',
 				'field' => 'username',
-				'value' => $username,
+				'value' => $dataForm['username'],
 			);
 
 			if(cekDuplikat($koneksi, $config_duplikat)){ // jika ada yg sama
-				$status = false;
-				$errorDb = false;
+				$status = $errorDb = false;
 				$duplikat = true;
-			}else{
+			}
+			else{
 				$duplikat = false;
+
+				// upload foto
+				if($foto){
+					$path = "../../assets/gambar/admin/$valueFoto";
+					if(!move_uploaded_file($foto['tmp_name'], $path)){
+						$pesanError['fotoError'] = "Upload Foto Gagal";
+						$status = $cekFoto = false;
+					}
+				}
+
+				if($cekFoto){					
+					// jika query berhasil
+					if(insertAdmin($koneksi, $dataForm)){
+						$status = true;
+						$errorDb = false;
+						session_start();
+						$_SESSION['notif'] = "Tambah Data Berhasil";
+					} 
+					else{
+						$status = false;
+						$errorDb = true;
+					}
+				}
 			}
 
 		}
 		else $status = false;
-			
+
 		$output = array(
 			'status' => $status,
 			'errorDb' => $errorDb,
@@ -156,6 +180,7 @@
 			'pesanError' => $pesanError,
 			'set_value' => $set_value,
 		);
+
 		echo json_encode($output);
 	}
 
@@ -166,40 +191,40 @@
 				'field' => $data['username'], 'label' => 'Username', 'error' => 'usernameError',
 				'value' => 'username', 'rule' => 'string | 1 | 10 | required',
 			),
-			// data password
-			array(
-				'field' => $data['password'], 'label' => 'Password', 'error' => 'passwordError',
-				'value' => 'password', 'rule' => 'string | 6 | 20 | required',
-			),
-			// data confirm password
-			array(
-				'field' => $data['konf_pass'], 'label' => 'Konfirm Password', 'error' => 'konf_passError',
-				'value' => 'konf_pass', 'rule' => 'string | 6 | 20 | required',
-			),
+			// // data password
+			// array(
+			// 	'field' => $data['password'], 'label' => 'Password', 'error' => 'passwordError',
+			// 	'value' => 'password', 'rule' => 'string | 6 | 20 | required',
+			// ),
+			// // data confirm password
+			// array(
+			// 	'field' => $data['confirm'], 'label' => 'Konfirm Password', 'error' => 'confirmError',
+			// 	'value' => 'confirm', 'rule' => 'string | 6 | 20 | required',
+			// ),
 			// data nama
 			array(
-				'field' => $data['id_barang'], 'label' => 'ID Barang', 'error' => 'id_barangError',
-				'value' => 'id_barang', 'rule' => 'string | 1 | 4 | required',
+				'field' => $data['nama'], 'label' => 'Nama', 'error' => 'namaError',
+				'value' => 'nama', 'rule' => 'string | 1 | 50 | required',
 			),
 			// data email
 			array(
-				'field' => $data['id_barang'], 'label' => 'ID Barang', 'error' => 'id_barangError',
-				'value' => 'id_barang', 'rule' => 'string | 1 | 4 | required',
+				'field' => $data['email'], 'label' => 'Email', 'error' => 'emailError',
+				'value' => 'email', 'rule' => 'email | 1 | 50 | required',
 			),
 			// data level
 			array(
-				'field' => $data['id_barang'], 'label' => 'ID Barang', 'error' => 'id_barangError',
-				'value' => 'id_barang', 'rule' => 'string | 1 | 4 | required',
+				'field' => $data['level'], 'label' => 'Level', 'error' => 'levelError',
+				'value' => 'level', 'rule' => 'string | 5 | 5 | required',
 			),
 			// data telp
 			array(
-				'field' => $data['id_barang'], 'label' => 'ID Barang', 'error' => 'id_barangError',
-				'value' => 'id_barang', 'rule' => 'string | 1 | 4 | required',
+				'field' => $data['telp'], 'label' => 'No. Telepon', 'error' => 'telpError',
+				'value' => 'telp', 'rule' => 'string | 1 | 15 | required',
 			),
 			// data alamat
 			array(
-				'field' => $data['id_barang'], 'label' => 'ID Barang', 'error' => 'id_barangError',
-				'value' => 'id_barang', 'rule' => 'string | 1 | 4 | required',
+				'field' => $data['alamat'], 'label' => 'Alamat', 'error' => 'alamatError',
+				'value' => 'alamat', 'rule' => 'string | 1 | 255 | required',
 			),
 		);
 
--- app/models/Admin_model.php
+++ app/models/Admin_model.php
@@ -9,3 +9,24 @@
 
 		return $result;
 	}
+
+	// insert barang
+	function insertAdmin($koneksi, $data){
+		$status = "1";
+		$query = "INSERT INTO admin (username, password, nama, email, telp, alamat, foto, level, status) VALUES (:username, :password, :nama, :email, :telp, :alamat, :foto, :level, :status)";
+
+		$statement = $koneksi->prepare($query);
+		$statement->bindParam(':username', $data['username']);
+		$statement->bindParam(':password', $data['password']);
+		$statement->bindParam(':nama', $data['nama']);
+		$statement->bindParam(':email', $data['email']);
+		$statement->bindParam(':telp', $data['telp']);
+		$statement->bindParam(':alamat', $data['alamat']);
+		$statement->bindParam(':foto', $data['foto']);
+		$statement->bindParam(':level', $data['level']);
+		$statement->bindParam(':status', $status);
+		$result = $statement->execute();
+		tutup_koneksi($koneksi);
+
+		return $result;
+	}
