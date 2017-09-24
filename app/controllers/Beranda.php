b6c4ba97820a630f3aba0eefeb518c374c211315
Ramadan Saputra <rarasta27@gmail.com>
2017-09-24 16:38:58 +0700

    controller/beranda
    => controller --> belum beres semua
    baru get data panel info (transaksi penjualan, pembelian), get data chart penjualan-laba, get data item terlaris dan kurang laku
    => view --> belum sempat binding data, yg baru di binding, panel info dan chart penjualan-laba

--- app/controllers/Beranda.php
+++ app/controllers/Beranda.php
@@ -6,13 +6,24 @@
 	include_once("../function/koneksi.php");
 	include_once("../function/validasi_form.php");
 	include_once("../function/datatable.php");
+	include_once("../models/Penjualan_model.php");
+	include_once("../models/Pembelian_model.php");
+	include_once("../models/Stok_model.php");
 
 	$action = isset($_POST['action']) ? $_POST['action'] : false;
 	if(!$action) die("Dilarang Akses Halaman Ini !!");
 	else{
 		switch ($action) {
-			case 'value':
-				# code...
+			case 'get_panel_info':
+				get_panel_info($koneksi);
+				break;
+
+			case 'get_chart_penjualan_laba':
+				get_chart_penjualan_laba($koneksi);
+				break;
+
+			case 'get_panel_item':
+				get_panel_item($koneksi);
 				break;
 			
 			default:
@@ -20,4 +31,140 @@
 				break;
 		}
 	}
+
+	function get_panel_info($koneksi){
+		$date = array(
+			'bulan' => date("m"),
+			'tahun' => date("Y"),
+		);
+
+		// get data total transaksi penjualan bulan ini
+		$get_penjualan = get_ket_penjualan($koneksi, $date, 'transaksi_penjualan');
+		$data_penjualan = (empty($get_penjualan['total'])) ? 
+			array(
+				'total' => '0', 
+				// 'bulan' => get_bulanIndo($date['bulan']), 
+				// 'tahun' => $date['tahun'],
+				'label' => 'Total Transaksi <br>Penjualan '.get_bulanIndo($date['bulan']).' '.$date['tahun'],
+			)
+			: 
+			array(
+				'total' => $get_penjualan['total'], 
+				// 'bulan' => get_bulanIndo($get_penjualan['bulan']), 
+				// 'tahun' => $get_penjualan['tahun'],
+				'label' => 'Total Transaksi <br>Penjualan '.get_bulanIndo($get_penjualan['bulan']).' '.$get_penjualan['tahun'],
+			);
+		
+		// get data total transaksi pembelian bulan ini
+		$get_pembelian = get_ket_pembelian($koneksi, $date, 'transaksi_pembelian');
+		$data_pembelian = (empty($get_pembelian['total'])) ? 
+			array(
+				'total' => '0',
+				// 'bulan' => get_bulanIndo($date['bulan']), 
+				// 'tahun' => $date['tahun'],
+				'label' => 'Total Transaksi <br>Pembelian '.get_bulanIndo($date['bulan']).' '.$date['tahun'],
+			) 
+			: 
+			array(
+				'total' => $get_pembelian['total'], 
+				// 'bulan' => get_bulanIndo($get_pembelian['bulan']), 
+				// 'tahun' => $get_pembelian['tahun'],
+				'label' => 'Total Transaksi <br>Pembelian '.get_bulanIndo($get_pembelian['bulan']).' '.$get_pembelian['tahun'],
+			);
+
+		// get data total item reject bulan ini
+		// $get_reject = get_ket_reject($koneksi, $date, 'total_reject');
+		// $data_reject = (!$get_reject || empty($get_reject)) ? "0" : rupiah($get_reject);
+
+		// // get data total item return bulan ini
+		// $get_return = get_ket_reject($koneksi, $date, 'total_return');
+		// $data_return = (!$get_return || empty($get_return)) ? "0" : rupiah($get_return);
+
+		$output = array(
+			'panel_penjualan' => $data_penjualan,
+			'panel_pembelian' => $data_pembelian,
+			// 'panel_reject' => $data_reject,
+			// 'panel_return' => $data_return,
+		);
+
+		echo json_encode($output);
+	}
+
+	function get_chart_penjualan_laba($koneksi){
+		$tahun =  date("Y");
+		$get_penjualan_laba = get_penjualan_laba($koneksi, $tahun);
+		$label = $total_penjualan = $total_laba = array();
+		foreach($get_penjualan_laba as $value){
+			$label[] = get_bulanIndo($value['bulan']);
+			$total_penjualan[] = $value['total_penjualan'];
+			$total_laba[] = $value['total_laba'];
+		}
+
+		$output = array(
+			'labels' => $label,
+			'datasets' => array(
+					array(
+						"data" => $total_penjualan,
+						"label" => "Penjualan",
+						"borderColor" => "#3e95cd",
+						"backgroundColor" => "#3e95cd",
+						"fill" => false,
+					),
+					array(
+						"data" => $total_laba,
+						"label" => "Laba",
+						"borderColor" => "#8e5ea2",
+						"backgroundColor" => "#8e5ea2",
+						"fill" => false,
+					),
+				),
+		);
+
+		echo json_encode($output);
+	}
 	
+	function get_panel_item($koneksi){
+		$date = array(
+			'bulan' => date("m"),
+			'tahun' => date("Y"),
+		);
+		$get_item_terlaris = get_ket_penjualan_item($koneksi, $date, 'DESC', 5);
+		$data_item_terlaris = array();
+		$no_urut = 0;
+		foreach($get_item_terlaris as $value){
+			$no_urut++;
+			$dataRow = array();
+			$dataRow['no_urut'] = $no_urut;
+			$dataRow['kd_barang'] = $value['kd_barang'];
+			$dataRow['kode_barang'] = $value['kode_barang'];
+			$dataRow['nama'] = $value['nama'];
+			$dataRow['total'] = $value['total'];
+
+			$data_item_terlaris[] = $dataRow;
+		}
+
+		$get_item_kurang_laku = get_ket_penjualan_item($koneksi, $date, 'ASC', 5);
+		$data_item_kurang_laku = array();
+		$no_urut = 0;
+		foreach($get_item_kurang_laku as $value){
+			$no_urut++;
+			$dataRow = array();
+			$dataRow['no_urut'] = $no_urut;
+			$dataRow['kd_barang'] = $value['kd_barang'];
+			$dataRow['kode_barang'] = $value['kode_barang'];
+			$dataRow['nama'] = $value['nama'];
+			$dataRow['total'] = $value['total'];
+
+			$data_item_kurang_laku[] = $dataRow;
+		}
+
+
+		$output = array(
+			'terlaris' => $data_item_terlaris,
+			'kurang_laku' => $data_item_kurang_laku,
+			// 'bulan' => get_bulanIndo($date['bulan']), 
+			// 'tahun' => $date['tahun'],
+		);
+
+		echo json_encode($output);
+	}
--- app/views/beranda/dashboard.php
+++ app/views/beranda/dashboard.php
@@ -1,111 +1,324 @@
 <?php
-	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
+  	Defined("BASE_PATH") or die("Dilarang Mengakses File Secara Langsung");
 
-	$notif = isset($_SESSION['notif']) ? $_SESSION['notif'] : false;
-	unset($_SESSION['notif']);
+  	$notif = isset($_SESSION['notif']) ? $_SESSION['notif'] : false;
+  	unset($_SESSION['notif']);
 ?>
 
 <section class="content-header">
-	<h1>Beranda <small>Dashboard Admin</small></h1>
-	<ol class="breadcrumb">
-    	<li><a href="#"><i class="fa fa-dashboard"></i> Zaco Hijab</a></li>
+  	<h1>Beranda <small>Dashboard Admin</small></h1>
+  	<ol class="breadcrumb">
+	  	<li><a href="#"><i class="fa fa-dashboard"></i> Zaco Hijab</a></li>
         <li class="active">Beranda</li>
-  	</ol>
+    </ol>
 </section>
 <!-- content -->
 <section class="content">
-	<!-- panel 1 -->
-	<div class="row">
-		<!-- panel info penjualan -->
-		<div class="col-lg-3 col-xs-6">
-			<div class="small-box bg-aqua">
-				<div class="inner">
-					<h3 class="info-penjualan"></h3>
-					<p class="label-info-penjualan">Total Penjualan Bulan Ini</p>
-				</div>
-				<div class="icon">
-             		<i class="ion ion-bag"></i>
-	            </div>
-	            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-			</div>
-		</div>
-		<!-- panel info pembelian -->
-		<div class="col-lg-3 col-xs-6">
-          	<!-- small box -->
-          	<div class="small-box bg-green">
-            	<div class="inner">
-              		<h3 class="info-pembelian"></h3>
-              		<p class="label-info-pembelian">Total Pembelian Bulan Ini</p>
-            	</div>
-            	<div class="icon">
-              		<i class="ion ion-stats-bars"></i>
-            	</div>
-            	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          	</div>
+  	<!-- panel 1 -->
+  	<div class="row">
+    	<!-- panel info penjualan -->
+    	<div class="col-lg-3 col-xs-6">
+      		<div class="small-box bg-aqua">
+        		<div class="inner">
+          			<h3 class="info-penjualan"></h3>
+          			<p class="label-info-penjualan"></p>
+        		</div>
+        		<div class="icon">
+                	<i class="ion ion-bag"></i>
+              	</div>
+              	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+      		</div>
+    	</div>
+    	<!-- panel info pembelian -->
+    	<div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+        	<div class="small-box bg-green">
+              	<div class="inner">
+                  	<h3 class="info-pembelian"></h3>
+                  	<p class="label-info-pembelian"></p>
+              	</div>
+              	<div class="icon">
+                  	<i class="ion ion-stats-bars"></i>
+              	</div>
+              	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
         </div>
         <!-- panel info reject -->
         <div class="col-lg-3 col-xs-6">
-          	<!-- small box -->
-          	<div class="small-box bg-yellow">
-            	<div class="inner">
-              		<h3 class="info-reject"></h3>
-              		<p class="label-info-reject">Total Reject Bulan Ini</p>
-            	</div>
-            	<div class="icon">
-              		<i class="ion ion-stats-bars"></i>
-            	</div>
-            	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          	</div>
+            <!-- small box -->
+            <div class="small-box bg-yellow">
+              	<div class="inner">
+                  	<h3 class="info-reject"></h3>
+                  	<p class="label-info-reject">Total Item Reject <br>Bulan Ini</p>
+              	</div>
+              	<div class="icon">
+                  	<i class="ion ion-stats-bars"></i>
+              	</div>
+              	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
         </div>
         <!-- panel info return -->
         <div class="col-lg-3 col-xs-6">
-          	<!-- small box -->
-          	<div class="small-box bg-red">
-            	<div class="inner">
-              		<h3 class="info-return"></h3>
-              		<p class="label-info-return">Total Return Bulan Ini</p>
-            	</div>
-            	<div class="icon">
-             	 	<i class="ion ion-stats-bars"></i>
-            	</div>
-            	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-          	</div>
+            <!-- small box -->
+            <div class="small-box bg-red">
+              	<div class="inner">
+                  	<h3 class="info-return"></h3>
+                  	<p class="label-info-return">Total Item Return <br>Bulan Ini</p>
+              	</div>
+              	<div class="icon">
+                	<i class="ion ion-stats-bars"></i>
+              	</div>
+              	<a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
         </div>
-	</div>
-	<!-- panel 2 -->
-	<div class="row">
+  	</div>
+  	<!-- panel 2 -->
+  	<div class="row">
 		<!-- panel grafik penjualan dan laba -->
-		<div class="col-xs-12 col-md-12">
-			<div class="box box-default collapsed-box">
-				<div class="box-header with-border">
-					<h3 class="box-title">Grafik Perbandingan Penjualan dan Laba</h3>
-					<div class="box-tools pull-right">
-                		<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+    	<div class="col-xs-12 col-md-8">
+      		<div class="box box-default box-penjualan-laba">
+        		<div class="box-header with-border">
+          			<h3 class="box-title">Grafik Perbandingan Penjualan dan Laba</h3>
+          			<div class="box-tools pull-right">
+                    	<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
               		</div>
-				</div>
-				<!-- grafik -->
-				<div class="box-body">
-              		The body of the box
-            	</div>
-			</div>
-		</div>
-	</div>
-	<!-- panel 3 -->
-	<div class="row">
-		<!-- panel grafik pengeluaran -->
-		<div class="col-xs-12 col-md-12">
-			<div class="box box-default collapsed-box">
-				<div class="box-header with-border">
-					<h3 class="box-title">Grafik Pengeluaran Tahun Ini</h3>
-					<div class="box-tools pull-right">
-                		<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+        		</div>
+        		<!-- grafik -->
+        		<div class="box-body">
+        			<div class="chart">
+              			<canvas id="chart_penjualan_laba"></canvas>
               		</div>
-				</div>
-				<!-- grafik -->
-				<div class="box-body">
-              		The body of the box
-            	</div>
-			</div>
-		</div>
+          		</div>
+          		<div class="overlay" style="display: none;">
+                    <i class="fa fa-refresh fa-spin"></i>
+                </div>
+      		</div>
+    	</div>
+    	<!-- panel item -->
+    	<div class="col-xs-12 col-md-4">
+      		<div class="box box-default box-keterangan-item">
+        		<div class="box-header with-border">
+          			<h3 class="box-title">Keterangan Item</h3>
+          			<div class="box-tools pull-right">
+                    	<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+              		</div>
+        		</div>
+        		<!-- grafik -->
+        		<div class="box-body">
+              		<div class="nav-tabs-custom">
+		    			<ul class="nav nav-tabs">
+		              		<li class="active"><a href="#tab_terlaris-1" data-toggle="tab">Terlaris</a></li>
+             				<li><a href="#tab_kurang_laku" data-toggle="tab">Kurang Laku</a></li>
+             				<!-- <li><a href="#tab_reject" data-toggle="tab">Reject</a></li>
+             				<li><a href="#tab_return" data-toggle="tab">Return</a></li> -->
+		    			</ul>
+		    			<div class="tab-content">
+		    				<!-- content terlaris -->
+		    				<div class="tab-pane active" id="tab_terlaris">
+		    				</div>
+		    				<!-- konten kurang laku -->
+		    				<div class="tab-pane" id="tab_kurang_laku">
+		    				</div>
+		    			</div>
+		    		</div>
+          		</div>
+          		<div class="overlay" style="display: none;">
+                    <i class="fa fa-refresh fa-spin"></i>
+                </div>
+      		</div>
+    	</div>
 	</div>
+  	<!-- panel 3 -->
+  	<div class="row">
+    	<!-- panel grafik pengeluaran -->
+    	<div class="col-xs-12 col-md-8">
+      		<div class="box box-default box-pembelian-pengeluaran">
+        		<div class="box-header with-border">
+          			<h3 class="box-title">Grafik Pembelian-Pengeluaran</h3>
+          			<div class="box-tools pull-right">
+                    	<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  	</div>
+        		</div>
+        		<!-- grafik -->
+        		<div class="box-body">
+            		The body of the box
+        		</div>
+        		<div class="overlay" style="display: none;">
+                    <i class="fa fa-refresh fa-spin"></i>
+                </div>
+      		</div>
+    	</div>
+    	<!-- panel stok warning -->
+    	<div class="col-xs-12 col-md-4">
+    		<div class="box box-default box-stok-warning">
+    			<div class="box-header with-border">
+          			<h3 class="box-title">Stok Warning</h3>
+          			<div class="box-tools pull-right">
+                    	<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                  	</div>
+        		</div>
+        		<!-- grafik -->
+        		<div class="box-body">
+            		The body of the box
+        		</div>
+        		<div class="overlay" style="display: none;">
+                    <i class="fa fa-refresh fa-spin"></i>
+                </div>
+    		</div>
+    	</div>
+  	</div>
 </section>
+
+<!-- js -->
+<script type="text/javascript" src="<?= base_url."assets/plugins/chartjs/Chart.min.js" ?>"></script>
+<script type="text/javascript">
+    var base_url = "<?php print base_url; ?>";
+</script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		// load panel info
+		load_panel_info();
+		// load panel item
+		load_panel_item();
+		// load chart penjualan-laba
+		var chart_penjualan_laba = $("#chart_penjualan_laba")[0].getContext('2d');
+		load_penjualan_laba(function(data){
+			var chart = new Chart(chart_penjualan_laba, {
+				type: 'line',
+				data: data,
+				options: {
+					title: {
+			        	display: true,
+			        	text: 'Perbandingan Laba dan Penjualan'
+			     	},
+			     	tooltips: {
+			           	// mode: 'label',
+			           	label: 'mylabel',
+			           	callbacks: {
+			               	label: function(tooltipItem, data){
+		                   		return 'Rp. '+tooltipItem.yLabel.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "."); 
+		                   	},
+		               	},
+			        },
+			     	scales: {
+			            yAxes: [{
+			                ticks: {
+		                    	callback: function(label, index, labels) { return label/1000; },
+			                    beginAtZero:true,
+			                    fontSize: 10,
+			                },
+			                scaleLabel: { 
+			                    display: true,
+			                    labelString: 'Rupiah (K)',
+			                    // fontSize: 10,
+			                }
+			            }],
+			            xAxes: [{
+			                ticks: {
+			                    beginAtZero: true,
+			                    // fontSize: 10
+			                },
+			               //  gridLines: {
+			               //      display:false
+			               //  },
+			                scaleLabel: {
+			                    display: true,
+			                    labelString: 'Bulan',
+			                    // fontSize: 10,
+			               	}
+			            }]
+			        }
+				}
+			});
+		});
+
+	});
+
+	// function load all panel
+	function load_panel_info(){
+		$.ajax({
+			url: base_url+"app/controllers/Beranda.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				"action": "get_panel_info"
+			},
+			success: function(data){
+				console.log(data);
+				setValue_panelInfo(data);
+			},
+			error: function (jqXHR, textStatus, errorThrown){ // error handling
+	            swal("Pesan Error", "Operasi Gagal, Silahkan Coba Lagi", "error");
+	            // clearBarang();
+	            console.log(jqXHR, textStatus, errorThrown);
+	        }
+		})
+	}
+
+  	// function load char penjualan-laba
+  	function load_penjualan_laba(handleData){
+  		$.ajax({
+			url: base_url+"app/controllers/Beranda.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				"action" : "get_chart_penjualan_laba"
+			},
+			beforeSend: function(){
+	            $('.box-penjualan-laba .overlay').css('display', 'block');
+	        },
+			success: function(data){
+				$('.box-penjualan-laba .overlay').css('display', 'none');
+				handleData(data);
+				console.log(data);
+			},
+			error: function (jqXHR, textStatus, errorThrown){
+	            swal("Pesan Error", "Operasi Gagal, Silahkan Coba Lagi", "error");
+	            $('.box-penjualan-laba .overlay').css('display', 'none');
+	            console.log(jqXHR, textStatus, errorThrown);
+	        } 
+		})
+  	}
+
+  	// funtion load panel keterang item
+  	function load_panel_item(){
+  		$.ajax({
+			url: base_url+"app/controllers/Beranda.php",
+			type: "post",
+			dataType: "json",
+			data: {
+				"action": "get_panel_item"
+			},
+			success: function(data){
+				console.log(data);
+				// set_value_panel(data);
+			},
+			error: function (jqXHR, textStatus, errorThrown){ // error handling
+	            swal("Pesan Error", "Operasi Gagal, Silahkan Coba Lagi", "error");
+	            // clearBarang();
+	            console.log(jqXHR, textStatus, errorThrown);
+	        }
+		})
+  	}
+
+	// set value panel info
+	function setValue_panelInfo(data){
+	    // panel info penjualan
+		$('.info-penjualan').text(data.panel_penjualan.total);
+		$('.label-info-penjualan').html(data.panel_penjualan.label);
+
+	    // panel info pembelian
+	    $('.info-pembelian').text(data.panel_pembelian.total);
+	    $('.label-info-pembelian').html(data.panel_pembelian.label);
+
+	    // panel info reject
+
+	    // panel info return
+	}
+
+	// set value panel item
+	function setValue_panelItem(data){
+		
+	}
+
+</script>
