322fe481c31a7eb5553000a6fd72f06cba445bea
Unknown <rezzarediza@gmail.com>
2017-11-18 14:37:35 +0700

    commit

--- app/views/pengeluaran/form.php
+++ app/views/pengeluaran/form.php
@@ -21,135 +21,131 @@
 
  <!-- isi konten -->
 <section class="content">
-    <div class="row">
-        <div class="col-xs-12">
-            <!-- panel box -->
-            <div class="box">
-                <!-- judul panel box -->
-                <div class="box-header">
-                    <div class="row">
-                        <div class="col-sm-6 col-xs-12">
-                            <h3 class="box-title">Form Data Pengeluaran</h3>
+    <form enctype="multipart/form-data" role="form" id="form_pengeluaran">
+        <input type="hidden" name="id" id="id">
+        <!-- panel 1 -->
+        <div class="row">
+            <div class="col-md-12">
+                <!-- panel data pegeluran -->
+                <div class="box">
+                    <div class="box-body">
+                        <div class="col-md-6">
+                            <fieldset>
+                                <legend>Data Pengeluaran</legend>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <!-- kode pengeluran -->
+                                        <div class="form-group field-kode">
+                                            <label for="fKd_pembelian">Kode Pengeluaran</label>
+                                            <input type="text" name="fKd_pengeluaran" id="fKd_pengeluaran" class="form-control" placeholder="Masukkan Kode Pengeluaran" readonly>
+                                            <span class="help-block small"></span>  
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <!-- tanggal -->
+                                        <div class="form-group field-tgl">
+                                            <label for="fTgl">Tanggal</label>
+                                            <input type="text" name="fTgl" id="fTgl" class="form-control datepicker">
+                                            <span class="help-block small"></span> <!-- Tampilan Validasi -->
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group field-jenis">
+                                    <label for="fJenis">Jenis Pengeluaran</label>
+                                    <select id="fJenis" name="fJenis" class="form-control" style="width: 100%;">
+                                        <option value="">-- Pilih Jenis Pengeluaran --</option>
+                                    </select>
+                                    <span class="help-block small"></span>
+                                </div>       
+                            </fieldset>
                         </div>
-                    </div>
-                </div>
-                <!-- isi panel box -->
-                <div class="box-body">
-                    <div class="row">
-                    	<form enctype="multipart/form-data" role="form">
-                    		<!-- fieldset data pengeluaran -->
-                    		<div class="col-md-6 col-xs-12">
-                    			<!-- Fieldset Data Pengeluaran -->
-                    			<fieldset>
-                        			<legend>Data Pengeluaran</legend>
-                        			<!-- kode pengeluaran -->
-                        			<div class="form-group">
-                        				<label for="fKd_pengeluaran">Kode Pengeluaran</label>
-                        				<input type="text" name="fKd_pengeluaran" id="fKd_pengeluaran" class="form-control" placeholder="Masukkan Kode Pengeluaran">
-                        			</div>
-
-                        			<!-- tanggal -->
-                        			<div class="form-group">
-                        				<label for="fTgl">Tanggal</label>
-                        				<input type="text" name="fTgl" id="fTgl" class="form-control datepicker">
-                        			</div>
-
-                        			<!-- jenis pengeluaran -->
-                        			<div class="form-group">
-                        				<label for="fJenis">Jenis Pengeluaran</label>
-                        				<select id="fJenis" name="fJenis" class="form-control" style="width: 100%;">
-                        					<option value="">-- Pilih Jenis Pengeluaran --</option>
-                        				</select>
-                        			</div>
-
-                        			<!-- Keterangan -->
-                        			<div class="form-group">
-                        				<div class="row">
-                        					<div class="col-md-8">
-                        						<label for="fKet">Keterangan</label>
-		                        				<input type="text" name="fKet" id="fKet" class="form-control" placeholder="Masukkan Keterangan">
-                        					</div>
-                        					<div class="col-md-4">
-                        						<label for="fQty">Qty</label>
-		                        				<input type="number" id="fQty" name="fQty" class="form-control" min="0">
-                        					</div>
-                        				</div>
-                        			</div>
-
-
-                        			<!-- Nominal -->
-          							<div class="form-group">
-          								<div class="row">
-          									<div class="col-md-12 col-xs-12">
-          										<label for="fNominal">Nominal</label>
-          									</div>
-          								</div>
-          								<div class="row">
-          									<div class="col-md-12 col-xs-12">
-	          									<div class="input-group">
-	          										<span class="input-group-addon" style="background-color: #dd4b39; color: white;">
-	          											Rp. 
-	          										</span>
-	          										<input class="form-control" placeholder="Masukkan Nominal" id="fNominal" name="fNominal" type="text">
-	          										<span class="input-group-addon">,00</span>
-	          										<span class="input-group-btn">
-			                        					<button type="button" id="fTambah_pengeluaran" name="fTambah_pengeluaran" class="btn btn-default">
-			                        						<i class="fa fa-plus"></i>
-			                        					</button>
-			                        				</span>
-	          									</div>
-	       									</div>
-          								</div>		
-          							</div>
-                        		</fieldset>
-
-                        		
-
-
-                    		</div>
-                    		<div class="col-md-6 col-xs-12">
-                    			
-                    			<!-- fieldset list item -->
-                    			<div class="row">
-                    				<div class="col-md-12 col-xs-12">
-                    					<fieldset>
-		                        			<legend>List Item</legend>
-		                        			<div class="row">
-		                        				<div class="col-md-12">
-		                        					<table id="tabel_item_pengeluaran" class="table table-bordered table-hover">
-				                        				<thead>
-				                        					<tr>
-				                        						<th style="width: 15px">No</th>
-				                        						<th>Keterangan</th>
-				                        						<th>Nominal</th>
-				                        						<th>Qty</th>
-				                        						<th>Aksi</th>
-				                        					</tr>
-				                        				</thead>
-                                                        <tbody></tbody>
-				                        			</table>
-		                        				</div>
-		                        				<div class="col-md-12">
-		                        					<h4 class="text-right">Total: Rp. -,00</h4>
-		                        				</div>
-		                        			</div>	
-		                        		</fieldset>
-                    				</div>
-                    			</div>
-                    		</div>
-                        		
-                    	</form>
-                    </div>
-                </div>
-                <!-- footer box -->
-                <div class="box-footer text-right">
-                	<button type="button" class="btn btn-default btn-lg" onclick="addPengeluaran()"><i class="fa fa-plus"></i> Tambah</button>
-                	<button type="button" class="btn btn-default btn-lg">Batal</button>
+                        <div class="col-md-6">
+                            <fieldset>
+                                <legend>Detail</legend>
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <label for="fKet">Keterangan</label>
+                                            <input type="text" name="fKet" id="fKet" class="form-control" placeholder="Masukkan Keterangan">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label for="fQty">Qty</label>
+                                            <div class="input-group">
+                                                <input type="number" id="fQty" name="fQty" class="form-control" min="0" placeholder="Qty">
+                                                <span class="input-group-addon">pcs</span>
+                                            </div>
+                                        </div>  
+                                    </div>
+                                    <span class="help-block small"></span>
+                                </div>
+                                <!-- Nominal -->
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-12 col-xs-12">
+                                            <label for="fNominal">Nominal</label>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 col-xs-12">
+                                            <div class="input-group">
+                                                <span class="input-group-addon" style="background-color: #dd4b39; color: white;">
+                                                    Rp. 
+                                                </span>
+                                                <input class="form-control" placeholder="Masukkan Nominal" id="fNominal" name="fNominal" type="number" min="0">
+                                                <span class="input-group-addon">,00</span>
+                                                <span class="input-group-btn">
+                                                    <button type="button" id="fTambah_pengeluaran" name="fTambah_pengeluaran" class="btn btn-default">
+                                                        <i class="fa fa-plus"></i>
+                                                    </button>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <span class="help-block small"></span>  
+                                </div>
+                            </fieldset>
+                        </div>
+                    </div>        
                 </div>
             </div>
         </div>
-    </div>
-</section>
+        <!-- panel 2 -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-body">
+                        <fieldset>
+                            <legend>List Item</legend>
+                            <div class="table-responsive">
+                               <table id="tabel_item_pembelian" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th style="width: 15px">No</th>
+                                            <th>Keterangan</th>
+                                            <th>Nominal</th>
+                                            <th>Qty</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <!-- tbody agar bisa ditambah saat button +barang di klik -->
+                                    <tbody></tbody>
+                                </table>
+                            </div>
+                            <h4 id="tampilHarga" class="text-right">Total: Rp. -,00</h4>  
+                        </fieldset>
+                    </div>
+                    <div class="box-footer text-right">
+                        <button type="submit" class="btn btn-default btn-lg btn-flat" id="btnSubmit" value="<?= $btn ?>">
+                            <i class="fa fa-plus"></i> <?= ucfirst($btn); ?>
+                        </button>
+                        <a href="<?=base_url."index.php?m=pengeluaran&p=list" ?>" class="btn btn-default btn-lg btn-flat"><i class="fa fa-reply"></i>  Batal</a>
+                    </div>
+                </div>     
+            </div>
+        </div>
+    </form>
+</section>                      
+
 
 <!-- js -->
 <!-- js datepicker -->
